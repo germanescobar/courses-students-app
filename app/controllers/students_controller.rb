7bc3029e465a03a15503d65d71bf8ef45826e2fd
German Escobar <german.escobarc@gmail.com>
2017-04-24 20:52:42 -0500

    Create UI

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,24 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+    @courses = Course.all
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:name, course_ids: [])
+    end
+end
