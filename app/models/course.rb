8a006442d8793a522eb8355705ebf80c0f8107be
German Escobar <german.escobarc@gmail.com>
2017-04-24 20:01:17 -0500

    Create models

---
+++ app/models/course.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Course < ApplicationRecord
+  has_and_belongs_to_many :students
+end
---
+++ app/models/student.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Student < ApplicationRecord
+  has_and_belongs_to_many :courses
+end
